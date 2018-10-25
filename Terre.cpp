327d78814978955ea03d01a8ff2116f77a500113
Mika Stéphane <ravoavahymika@gmail.com>
2018-10-25 13:50:13 +0300

    Commit terre

--- Terre.cpp
+++ Terre.cpp
@@ -4,3 +4,16 @@ Vecteur3D Terre::gravite(Vecteur3D point) {
     double P = sqrt(pow(point[0], 2) + pow(point[1], 2) + pow(point[2], 2));
     return (- GM / pow(P, 3)) * point;
 }
+
+Terre::Terre() {
+
+}
+
+
+Terre::Terre(const Terre &) {
+
+}
+
+Terre::~Terre() {
+
+}
